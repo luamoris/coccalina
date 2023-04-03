7064325f787c470a7a445701328fdf844c742a1b
luamoris <yaiosifua@gmail.com>
2023-04-03 03:20:50 +0200

    add animation bg

--- js/main.js
+++ js/main.js
@@ -1,3 +1,95 @@
+//  Частицы Stars
+const bg = document.getElementById("bg");
+const starsCounter = 20;
+
+function getRandomInt(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function createStar() {
+   const star = document.createElement("span");
+   star.classList.add("star");
+   return star;
+}
+
+function sizeWindow() {
+   return {
+      widht: window.innerWidth,
+      height: window.innerHeight,
+   };
+}
+
+function initStar() {
+   const star = createStar();
+   let size = sizeWindow();
+   let point = { x: 0, y: 0 };
+   let x = 0, y = 0, speed = 0;
+   let width = 6, height = 6;
+   let xRandom, yRandom = 0;
+   const colors = ["_c_pink", "_c_yellow", "_c_blue", "_c_green"];
+   let color = colors[getRandomInt(0, colors.length)];
+   let right = getRandomInt(0, 2) == 0 ? true : false;
+   let top = getRandomInt(0, 2) == 0 ? true : false;
+
+   const createPoints = () => {
+      return {
+         x: getRandomInt(0, size.widht),
+         y: getRandomInt(0, size.height),
+      };
+   };
+
+   const init = () => {
+      x, y = 0, 0;
+      size = sizeWindow();
+      point = createPoints();
+      speed = getRandomInt(-1, 2);
+      width = getRandomInt(6, 11);
+      height = getRandomInt(6, 11);
+      color = colors[getRandomInt(0, colors.length)];
+      star.style.width = width + "px";
+      star.style.height = height + "px";
+      star.style.left = point.x + "px";
+      star.style.top = point.y + "px";
+      star.style.transform = `translate(0px, 0px)`;
+      right = getRandomInt(0, 2) == 0 ? true : false;
+      top = getRandomInt(0, 2) == 0 ? true : false;
+      xRandom = right ? getRandomInt(1, 3) : getRandomInt(-3, -1);
+      yRandom = top ? getRandomInt(1, 3) : getRandomInt(-3, -1);
+   };
+
+   init();
+   star.classList.add(color);
+   star.classList.add("_start");
+   let isPaused = false;
+
+   const move = setInterval(function () {
+      if (!isPaused) {
+         x = x + xRandom;
+         y = y + yRandom;
+         star.style.transform = `translate(${x}px, ${y}px)`;
+         if (point.x + x < 0 - width || point.x + x > size.widht || point.y + y < 0 - height || point.y + y > size.height) {
+            // console.log(point.x + x, point.y + y);
+            // clearInterval(move);
+            star.classList.remove("_start");
+            star.classList.remove(color);
+            star.classList.add("_end");
+            isPaused = true;
+            setTimeout(function () {
+               init();
+               star.classList.remove("_end");
+               star.classList.add(color);
+               star.classList.add("_start");
+               isPaused = false;
+            }, 2000);
+         }
+      }
+   }, 25);
+
+   return star;
+}
+
 // Current Year
 const yearElm = document.getElementById("year");
 const currentYear = (new Date()).getFullYear();
@@ -18,6 +110,15 @@ function getCLockUTC(utc) {
 
 clockElm.textContent = getCLockUTC(timezoneUTC);
 window.onload = function () {
+   let i = 0;
+   const createStars = setInterval(function () {
+      if (i > starsCounter) {
+         clearInterval(createStars)
+      }
+      bg.appendChild(initStar());
+      i++;
+   }, 1000);
+
    window.setInterval(function () {
       clockElm.textContent = getCLockUTC(timezoneUTC);
    }, 1000);
